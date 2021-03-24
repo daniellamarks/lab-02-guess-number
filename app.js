1e7c268e259be1393b33ec523b4cea82ffce40c5
Daniella Marks <marks.daniella@gmail.com>
2021-03-24 15:53:06 -0700

    added reset button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { compareNumbers } from './utils.js';
 const input = document.getElementById('user-input');
 const button = document.getElementById('guess-button');
 let guessOutput = document.getElementById('guess-output');
+const resetButton = document.getElementById('reset-button')
 
 const correctNumber = Math.ceil((Math.random() * 20));
 console.log(correctNumber)
@@ -16,19 +17,29 @@ console.log(triesRemaining)
 button.addEventListener('click', () => {
 
     triesRemaining--;
-    console.log(triesRemaining);
+    // console.log(triesRemaining);
 
     const inputAsNumber = Number(input.value);
-    console.log(inputAsNumber, correctNumber);
+    // console.log(inputAsNumber, correctNumber);
 
     const comparison = compareNumbers(inputAsNumber, correctNumber);    
-    guessOutput.textContent = `$(comparison);
+    guessOutput.textContent = `${comparison} You have ${triesRemaining} tries remaining.`;
 
     if (triesRemaining <= 0) { 
-        guessOutput.textContent = "Try again!";
+        guessOutput.textContent = "Sorry, you lost!";
         button.style.display = "none"
+        resetButton.classList.remove('hidden')
     }
+
+    if (inputAsNumber === correctNumber) {
+        guessOutput.textContent = `${comparison} You win!`;
+        button.style.display = "none";
+        resetButton.classList.remove('hidden');
+    }
+
 });
+
+
 
 
 // set event listeners to update state and DOM
--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 export function compareNumbers(guess, correctNumber) {
-    if (guess === correctNumber) return `you're spot on!`;
-    if (guess > correctNumber) return `you're number is too high, guess again`;
-    if (guess < correctNumber) return `you're number is too low, guess again`;
+    if (guess === correctNumber) return `You're spot on!`;
+    if (guess > correctNumber) return `You're number is too high, guess again.`;
+    if (guess < correctNumber) return `You're number is too low, guess again.`;
 }
 
